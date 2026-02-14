b98d2f2a22aeacdc1d26d3ff22a5e2348a512ca4
Mayank Kumar <mackcodes03@gmail.com>
2026-02-14 22:55:37 +0530

    completed the game(to be played in console & no UI)

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,4 +1,4 @@
-function getComputerChoice(){
+function getComputerChoice(){   // this function gets the computer choice
     let randomNumber = Math.floor(Math.random()*3);
     if(randomNumber === 0){
         return "rock";
@@ -11,9 +11,9 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
+function getHumanChoice(){  // it gets the human choice
     let userInput = prompt(" Rock, Paper, Scissor"); 
-    userInput = userInput.toLowerCase();
+    userInput = userInput.toLowerCase();  // this is to make the input case-insensitive
     if(userInput === "rock" || userInput ==="r"){
         return "rock";
     }
@@ -31,7 +31,7 @@ function getHumanChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice){  // play the round
     if(humanChoice === computerChoice){
         return `It's a DRAW!!!! You both chose ${humanChoice}.`;
     }
@@ -41,7 +41,7 @@ function playRound(humanChoice, computerChoice){
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
     humanScore++;
-    return `You WIN!!! ${humanChoice} beats ${computerChoice}`;
+    return `You WIN!!! ${humanChoice} beats ${computerChoice}`;  // fetch the choices
     }
     else {
         computerScore++;
@@ -49,7 +49,7 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
-function playGame(){
+function playGame(){   // this function make the game of 5 rounds
     humanScore = 0;
     computerScore = 0;
     for(let i = 0; i < 5; i++){
@@ -73,4 +73,4 @@ function playGame(){
 
             ${finalMessage}`);
     }
-playGame();
+playGame();  // executes the function
