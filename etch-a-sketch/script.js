a9a76d78b861818035d60f02e8a6192c0f00df01
Mayank Kumar <mackcodes03@gmail.com>
2026-02-18 01:35:07 +0530

    completed the project

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,62 @@
+const div = document.querySelector("#container");
+const button = document.querySelector("#button");
+const clearBtn = document.querySelector("#clear");
+
+
+let isDrawing = false;
+
+ function inputValidation(){
+    const userInput = (Number)(prompt("Enter the number of squares per side for the new grid.\nMaximum Input = 100."))
+    if(
+        userInput === null ||
+        isNaN(userInput) ||
+        userInput <= 0 ||
+        userInput > 100
+    ){
+        alert("Entered Input exceeds the Maximum limit!!! Please input within the input range.");
+        return;
+    } else {
+        createGrid(userInput);
+    }
+ }
+
+document.addEventListener("mousedown", () => {
+    isDrawing = true;
+});
+document.addEventListener("mouseup", () => {
+    isDrawing = false;
+});
+
+
+button.addEventListener("click", () => {
+    inputValidation();
+});
+clearBtn.addEventListener("click", () => {
+    clearSketchPad();
+});
+
+function clearSketchPad(){  //clears the sketch pad
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square =>{
+        square.style.backgroundColor = "";
+    })
+}
+
+function createGrid(size){
+    div.innerHTML = "";   //clearing the container so that custom grid can be added
+    for(let i = 0; i < size * size; i++){
+        const newDiv = document.createElement("div");
+        const squareSize = 960/size;   // dynamically adjust the size so that the sletch pad remains a square always
+        newDiv.style.width = squareSize+"px";  
+        newDiv.style.height = squareSize+"px";
+        newDiv.classList.add("square");
+        newDiv.addEventListener("mouseover", () => {
+            if(isDrawing){
+                newDiv.style.backgroundColor = "red";
+            }
+         })
+        div.appendChild(newDiv);
+    }
+}
+
+createGrid(16);  // default grid structure of the page
